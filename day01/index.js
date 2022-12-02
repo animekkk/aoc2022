7221126171682b568ec0151d52914d17ceb32c62
animekkk <animekgaming@gmail.com>
2022-12-02 16:15:42 +0100

    Little fix

--- day01/index.js
+++ day01/index.js
@@ -4,30 +4,29 @@ const input = fs.readFileSync('./input.txt', { encoding: 'utf-8' });
 
 let elves = [];
 
-let elf = { calories: [], totalCalories: 0 };
+let elf = { calories: 0 };
 input.split('\n').forEach((line) => {
 	if (line.length === 1) {
 		elves.push(elf);
-		elf = { calories: [], totalCalories: 0 };
+		elf = { calories: 0 };
 	} else {
 		const kcal = Number.parseInt(line);
-		elf.calories.push(kcal);
-		elf.totalCalories += kcal;
+		elf.calories += kcal;
 	}
 });
 elves.push(elf); // Push last elf
 
-elves = elves.sort((a, b) => b.totalCalories - a.totalCalories);
+elves = elves.sort((a, b) => b.calories - a.calories);
 
 // Part 1
 
 const mostCalories = elves[0];
-console.log(mostCalories.totalCalories);
+console.log(mostCalories.calories);
 
 // Part 2
 
 let topThreeCalories = 0;
 for (let i = 0; i < 3; i++) {
-	topThreeCalories += elves[i].totalCalories;
+	topThreeCalories += elves[i].calories;
 }
 console.log(topThreeCalories);
